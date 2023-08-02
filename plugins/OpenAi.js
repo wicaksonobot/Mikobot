0bd8206ec422d7cbd247f60efc31cfb423d3dee2
wicaksonobot <140737444+wicaksonobot@users.noreply.github.com>
2023-08-02 16:47:03 +0700

    Update OpenAi.js

--- plugins/OpenAi.js
+++ plugins/OpenAi.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { text,  usedPrefix,  command }) => {
     if (!text) throw `Hay adakah yang bisa saya bantu??`
 await m.reply('Matte Onichan...')
-let mikobot = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${global.lolkey}&text=${text}&user=user-unique-id`)
+let mikobot = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=${global.lolkey}&text=${text}&user=user-unique-id`)
 let hasil = await mikobot.json()
  m.reply(`${hasil.result}`.trim())
     }  
